38b73d3dd85d843ba0f9cb89620bfb38243498db
PragyaChoubey <142969929+PragyaChoubey@users.noreply.github.com>
2024-08-15 21:37:30 +0530

    Add files via upload

---
+++ drumkit.js
@@ -0,0 +1,15 @@
+window.addEventListener('keydown',(e)=>{
+var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+var key = document.querySelector(`key[data-key="${e.keyCode}"]`);
+if(audio){
+    audio.play();
+    audio.currentTime=0;
+     key.classList.add('active');
+}
+setInterval(() => {
+    key.classList.remove('active');
+
+},300);
+})
+
+
